21ddec0da0997d106b465ff1c7e90ed869d6605e
pescador95 <iedio_junior@hotmail.com>
2022-10-15 20:50:48 -0300

    Efetuado alteracao do localstorage para o sessionstorage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,10 +1,10 @@
-export const setLocalStorage = (key, value) => {
-  return localStorage.setItem(key, JSON.stringify(value));
+export const setSessionStorage = (key, value) => {
+  return sessionStorage.setItem(key, JSON.stringify(value));
 };
 
-export const getLocalStorage = (key, defaultValue) => {
+export const getSessionStorage = (key, defaultValue) => {
   try {
-    const response = localStorage.getItem(key);
+    const response = sessionStorage.getItem(key);
 
     const parsed = JSON.parse(response);
 
